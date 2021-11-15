c844a548b5a58f3cbc33f336595a007fe355c8dd
Dustin Neumann <dneumann@mhighpoint.com>
2021-11-14 19:50:49 -0700

    Added gdot

--- config.el
+++ config.el
@@ -3,6 +3,8 @@
 (tool-bar-mode -1)
 (load-theme 'adwaita t)
 
+(message "Configuring package archives")
+
 (require 'package)
 
 (setq package-archives 
@@ -11,7 +13,8 @@
     ("melpa"     . "http://melpa.org/packages/")))
 
 (package-initialize)
-(package-refresh-contents)
+
+;; (package-refresh-contents)
 
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
@@ -20,9 +23,37 @@
 
 (setq use-package-always-ensure t)
 
+(use-package sexy-monochrome-theme
+  :config
+  (load-theme 'sexy-monochrome t))
+
+(setq make-backup-files nil)
+
 (use-package which-key
-  :diminish
-  :defer 5
   :config (which-key-mode)
           (which-key-setup-side-window-bottom)
-          (setq which-key-idle-delay 0.05))
+          (setq which-key-idle-delay 0.5))
+
+(use-package magit
+  :config (global-set-key (kbd "C-x g") 'magit-status))
+
+(use-package org-bullets
+  :hook (org-mode . org-bullets-mode))
+
+(use-package f) ; f-delete, f-mkdir, f-move, f-bexists?, f-hidden?
+
+;; (setq org-bullets-bullet-list '("\u200b"))
+
+(require 'org)
+
+(custom-set-faces
+ '(org-document-title ((t (:weight thin 2.0))))
+ '(org-level-1 ((t (:inherit header-line :height 2.0))))
+ '(org-level-2 ((t (:inherit header-line :height 1.8))))
+ '(org-level-3 ((t (:inherit header-line :height 1.0))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
+
+(message "Set the custom face")
+
+(use-package lispy)
--- init.el
+++ init.el
@@ -19,10 +19,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit which-key use-package)))
+ '(package-selected-packages
+   '(straight-el straight lispy sexy-monochrome-theme sexy-monochrome f org-bullets magit which-key use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-document-title ((t (:weight thin 2.0))))
+ '(org-level-1 ((t (:inherit header-line :height 2.0))))
+ '(org-level-2 ((t (:inherit header-line :height 1.8))))
+ '(org-level-3 ((t (:inherit header-line :height 1.0))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
